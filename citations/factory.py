5d1896d000e7d034ec994bf52b070a2dc5761aad
pacien <pacien.trangirard@pacien.net>
2019-01-13 16:43:35 +0100

    separate card and content parametrisation steps

--- citations/factory.py
+++ citations/factory.py
@@ -9,19 +9,17 @@ from . import defaults, themes
 class Factory:
     """
         :param width: width of the image, works better if it's even
-        :param height: minimal height of the image
+        :param min_height: minimal height of the image
         :param multiplier: image pixels size multiplier
 
         .. warnings:: The built in stamp only work with multiplier == 2
     """
-    def __init__(self, lines, penalty, title: str,
-                 barcode: List[int], theme=themes.named[defaults.theme],
-                 condensed: bool = False, use_alt_font: bool = False,
+    def __init__(self, theme=themes.named[defaults.theme], condensed: bool = False, use_alt_font: bool = False,
                  main_font: str = defaults.main_font_file, alt_font: str = defaults.alt_font_file,
                  stamp_filename=defaults.stamp_filename, stamp_background=defaults.stamp_bg_filename,
-                 width=defaults.width, height=defaults.height, multiplier: int = defaults.multiplier):
+                 width=defaults.width, min_height=defaults.height, multiplier: int = defaults.multiplier):
         self.width = width
-        self.height = height
+        self.min_height = min_height
         self.multiplier = multiplier
 
         self.theme = theme
@@ -49,107 +47,64 @@ class Factory:
         else:
             self.title_font = self.body_font
 
-        self.barcode_align = 0  # 0, 1 -> right, left
-        self.barcode = barcode
-
         self.title_align = 0
-        self.title = title
 
-        ### Those should be set manually
+        # 0, 1 -> right, left
+        if self._align_line(0) == 0:
+            self.barcode_align = 0
+        elif self._align_line(0) == 2:
+            self.barcode_align = 1
+
+        # Those should be set manually
         # WARNING: might not work properly with text wrapping! Proceed with caution
         self.body_align = 0  # 0, 1, 2 -> left, center, right
         self.wrap_by_char = False
         self.split_at_newline = False
         self.pattern = None
 
-        self.lines = lines
-
-        self.penalty = penalty
-
-    def _check_options(self, _skip_=False, lines=None, penalty=None, title=None,
-                       barcode=None, theme=None,
-                       condensed=None, use_alt_font=None):
-        # A small shortcut for sanity if the user of library really knows what they are doing
-        if _skip_:
-            return
-
-        if theme is not None:
-            self.theme = theme
-
-        self.use_alt_font = use_alt_font or self.use_alt_font
-        self.condensed = condensed or self.use_alt_font
-
-        self.min_lines = 3
-        self.line_height = 10
-        if self.condensed:
-            self.min_lines = 4
-            self.line_height = 9
-
-        self.body_font = ImageFont.truetype(self.body_font_file, 16)
-        if self.use_alt_font:
-            self.title_font = ImageFont.truetype(self.alt_font_file, 16)
-        else:
-            self.title_font = self.body_font
-
-        if self._align_line(0) == 0:
-            self.barcode_align = 0
-        if self._align_line(0) == 2:
-            self.barcode_align = 1
-
-        if barcode is not None:
-            self.barcode = barcode
-        if title is not None:
-            self.title = title
-        if lines is not None:
-            self.lines = lines
-        if penalty is not None:
-            self.penalty = penalty
-
-    def generate_file(self, filename, filetype="PNG", *options):
-        img = self.generate_image(*options)
+    def generate_file(self, filename, filetype='PNG', **options):
+        img = self.generate_image(**options)
         img.save(filename, filetype)
 
-    def generate_image(self, lines=None, *options):
-        if lines is not None:
-            self.lines = lines
-        self._check_options(lines, *options)
-        self.lines, self.height = self._process_lines(self.lines)
-        img = Image.new("RGB", (self.width * self.multiplier, self.height * self.multiplier), self.theme.background)
+    def generate_image(self, content: List[str], penalty: str, title: str, barcode: List[int] = ()):
+        lines, content_height = self._process_lines(content)
+        height = max(content_height, self.min_height)
+        img = Image.new('RGB', (self.width * self.multiplier, height * self.multiplier), self.theme.background)
         draw = ImageDraw.Draw(img)
-        self._generate(draw)
+        self._generate(draw, height, lines, penalty, title, barcode)
         return img
 
-    def _generate(self, draw: ImageDraw):
+    def _generate(self, draw: ImageDraw, height: int, lines: List[str], penalty: str, title: str, barcode: List[int]):
         """Internal method to draw on already generated image.
 
         Draws on supplied PIL.ImageDraw canvas. No promises if
         you use it on your own, the text might not fit. Use
         generate_file or generate_image instead.
         """
-        self._stamp(draw, self.stamp_img_bg)
-        self._stamp(draw, self.stamp_img, self.theme.background)
+        self._stamp(draw, height, self.stamp_img_bg)
+        self._stamp(draw, height, self.stamp_img, self.theme.background)
         self._dots_row(draw, 0, (0, 0))
-        self._roll(draw)
-        self._rect(draw, self.width - 1, 0, self.width, self.height, self.theme.details)
-        self._dots_row(draw, self.height - 1, (1, 0))
+        self._roll(draw, height)
+        self._rect(draw, self.width - 1, 0, self.width, height, self.theme.details)
+        self._dots_row(draw, height - 1, (1, 0))
 
-        self._print_barcode(draw, self.barcode)
+        self._print_barcode(draw, barcode)
 
         # Header separator line
         self._dots_row(draw, 17, (8, 10), self.theme.foreground)
-        self._text_line(draw, 0, self.title.upper())
+        self._text_line(draw, 0, title.upper())
 
         offset = 1
-        for line in range(len(self.lines)):
-            self._text_line(draw, line + offset, self.lines[line])
+        for index, line in enumerate(lines):
+            self._text_line(draw, index + offset, line)
 
         # Footer separator line
         if self.condensed:
-            self._dots_row(draw, self.height - 22 - 1, (8, 10), self.theme.foreground)
+            self._dots_row(draw, height - 22 - 1, (8, 10), self.theme.foreground)
         else:
-            self._dots_row(draw, self.height - 26 - 1, (8, 10), self.theme.foreground)
+            self._dots_row(draw, height - 26 - 1, (8, 10), self.theme.foreground)
 
-        self._footer(draw, self.penalty)
+        self._footer(draw, height, penalty)
 
     def _process_lines(self, input_lines):
         lines = []
@@ -171,7 +126,7 @@ class Factory:
                 newlines.append(out[0])
         lines = newlines
 
-        new_height = self.height + max(0, len(lines) - self.min_lines) * self.line_height
+        new_height = self.min_height + max(0, len(lines) - self.min_lines) * self.line_height
         return lines, new_height
 
     def _trim_line_length(self, text: str, font: ImageFont, margins: Tuple[int, int], wrap_by_char: bool = False,
@@ -251,8 +206,8 @@ class Factory:
         # The `- 1` here is to not have aliasing artifacts. Might
         # break if the chosen multiplier is not divisible by 2
 
-    def _roll(self, draw: ImageDraw):
-        for x in range((self.height + 3) // 9):
+    def _roll(self, draw: ImageDraw, height: int):
+        for x in range((height + 3) // 9):
             self._rect(draw,
                        2, 3 + 9 * x,
                        3, 3)
@@ -295,27 +250,25 @@ class Factory:
                 draw.text((x * mult - 1, offset * mult), text,
                           font=font, fill=color)
 
-    def _stamp(self, draw: ImageDraw, img: Image, color: str = None):  # 32x32 at 150x44
+    def _stamp(self, draw: ImageDraw, height: int, img: Image, color: str = None):  # 32x32 at 150x44
         if color is None:
             color = self.theme.details
-        draw.bitmap(((self.width * self.multiplier - img.width) // 2 - 1, (self.height - 4 - 32) * self.multiplier),
+        draw.bitmap(((self.width * self.multiplier - img.width) // 2 - 1, (height - 4 - 32) * self.multiplier),
                     img, color)
 
-    def _footer(self, draw: ImageDraw, text: str, color: str = None):
+    def _footer(self, draw: ImageDraw, height: int, text: str, color: str = None):
         m = self.multiplier
         if color is None:
             color = self.theme.foreground
         text = text.upper()
-        y = (self.height - 15) * m
+        y = (height - 15) * m
         size = self.body_font.getsize(text)
 
         x = math.ceil((self.width - 6 - 10 - (size[0] - 1) // m) / 2) * m + 6 * m
         draw.text((x - 1, y), text, font=self.body_font, fill=color)
         return
 
-    def _print_barcode(self, draw: ImageDraw, spec: List[int] = None, color: str = None):
-        if spec is None:
-            spec = self.barcode
+    def _print_barcode(self, draw: ImageDraw, spec: List[int], color: str = None):
         if color is None:
             color = self.theme.foreground
         end = self.width - 11
